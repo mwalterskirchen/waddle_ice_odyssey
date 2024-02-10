46253454e202d7876eb8bf59893dcf580ca9e2a9
Maximilian Walterskirchen <mw@functn.com>
2024-02-10 10:29:15 +0100

    adding tile sets, adding level 1 and basic HUD and BG music

---
+++ src/UI/Heartbar.gd
@@ -0,0 +1,20 @@
+extends HBoxContainer
+
+enum STATES {full, partial, empty}
+
+func get_cropped_texture(texture : Texture, region : Rect2) -> AtlasTexture:
+	var atlas_texture := AtlasTexture.new()
+	atlas_texture.set_atlas(texture)
+	atlas_texture.set_region(region)
+	return atlas_texture
+	
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
